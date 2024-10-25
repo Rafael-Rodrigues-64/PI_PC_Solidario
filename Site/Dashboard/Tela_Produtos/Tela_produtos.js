0905c7e72c7e0dac7fb653eda251666dc1fee686
Larissa Moreira <larimoreirak02@gmail.com>
2024-10-24 21:25:17 -0300

    Fazendo ajustes em produtos e pesquisa

--- Site/Dashboard/Tela_Produtos/Tela_produtos.js
+++ Site/Dashboard/Tela_Produtos/Tela_produtos.js
@@ -8,20 +8,40 @@ document.getElementById('selectAll').addEventListener('change', function() {
 let currentPage = 1;
 const totalPages = 100;  // Defina o número total de páginas
 
-document.getElementById('totalPages').innerText = totalPages;
+const doadoCheckbox = document.getElementById('selectDoado');
+const naoDoadoCheckbox = document.getElementById('selectNaoDoado');
+const allItems = document.querySelectorAll('.estoque .item');
 
-document.getElementById('prevPage').addEventListener('click', function() {
-    if (currentPage > 1) {
-        currentPage--;
-        document.getElementById('currentPage').innerText = currentPage;
-        // Aqui você pode adicionar a lógica para atualizar a lista de produtos da página
+// Função para mostrar apenas os itens doados
+doadoCheckbox.addEventListener('change', function() {
+    if (this.checked) {
+        allItems.forEach(item => {
+            if (item.classList.contains('doado')) {
+                item.style.display = 'block';
+            } else {
+                item.style.display = 'none';
+            }
+        });
+    } else {
+        allItems.forEach(item => {
+            item.style.display = 'block';
+        });
     }
 });
 
-document.getElementById('nextPage').addEventListener('click', function() {
-    if (currentPage < totalPages) {
-        currentPage++;
-        document.getElementById('currentPage').innerText = currentPage;
-        // Aqui você pode adicionar a lógica para atualizar a lista de produtos da página
+// Função para mostrar apenas os itens não doados
+naoDoadoCheckbox.addEventListener('change', function() {
+    if (this.checked) {
+        allItems.forEach(item => {
+            if (item.classList.contains('nao-doado')) {
+                item.style.display = 'block';
+            } else {
+                item.style.display = 'none';
+            }
+        });
+    } else {
+        allItems.forEach(item => {
+            item.style.display = 'block';
+        });
     }
 });
