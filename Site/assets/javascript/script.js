3f9a85360de87fc83c82e1c764bc562030932e29
Silvia Silva <silviapantoja497@gmail.com>
2025-02-17 19:59:16 -0300

    subindo atualização

--- Site/assets/javascript/script.js
+++ Site/assets/javascript/script.js
@@ -7,7 +7,7 @@ document.getElementById('radio1').checked = true
 		proximaImg()
 	}, 6000)
 
-function proximaImg(){
+function proximaImg(){}
 	cont++
 
 	if(cont > 3){
@@ -15,12 +15,14 @@ function proximaImg(){
 	}
 
 	document.getElementById('radio'+cont).checked = true
-}
-const btnMobile = Document.getElementById('btn-mobile');
-function ToggleMenu(){
-	const nav = document.getElementById('nav');
-	nav.classList.toggle('ative');
-}
+
+// }
+// const btnMobile = Document.getElementById('btn-mobile');
+// function ToggleMenu(){
+// 	const nav = document.getElementById('nav');
+// 	nav.classList.toggle('ative');
+// }
 
 
-btnMobile.addEventlistEner('click',ToggleMenu )
+// btnMobile.addEventlistEner('click',ToggleMenu )
+
